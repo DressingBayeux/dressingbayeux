1753d390f57d95461cb99aa308230cc96ebfb6a2
Le Dressing <lemercier7.victor7@gmail.com>
2022-05-26 16:12:00 +0200

    final commit

--- index.js
+++ index.js
@@ -13,3 +13,32 @@ $(window).on('load',function(){
         }); 
     },1000);
 });
+
+// burger
+
+
+let burgeropen = document.getElementById("openburger");
+
+function closingburger(event) {
+    openburger.hidden = false;
+    event.preventDefault();
+}
+
+function openingburger(event) {
+  openburger.hidden = true;
+  event.preventDefault();
+}
+
+// contact
+
+let contactopen = document.getElementById("opencontact");
+
+function closingcontact(event) {
+    opencontact.hidden = false;
+    event.preventDefault();
+}
+
+function openingcontact(event) {
+  opencontact.hidden = true;
+  event.preventDefault();
+}
